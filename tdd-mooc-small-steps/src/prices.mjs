b3e98d63304493f9df063136712b42c765b3cbd4
Ville Karvonen <villekarvonen1@gmail.com>
2023-10-10 13:21:30 +0300

    5

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -1,3 +1,4 @@
+import { Temporal } from "@js-temporal/polyfill";
 import "./polyfills.mjs";
 import express from "express";
 
@@ -25,7 +26,7 @@ function createApp(database) {
 
   function parseDate(dateString) {
     if (dateString) {
-      return new Date(dateString);
+      return Temporal.PlainDate.from(dateString);
     }
   }
 
