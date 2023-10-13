43c5ecc535afbc9debd77a0ad981f62fc220bf4e
Ville Karvonen <villekarvonen1@gmail.com>
2023-10-13 08:30:56 +0300

    [GREEN]

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -4,10 +4,12 @@ export class Board {
   currentBlock;
   currentBlockX;
   currentBlockY;
+  falling;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.falling = false;
   }
   
   toString() {    
@@ -21,17 +23,23 @@ export class Board {
   drop(arg){
     if(arg === 'Y') {
       throw new Error("already falling");
+    }else{
+      this.falling = true;
+      this.currentBlock = arg
+      this.currentBlockX = 1
+      this.currentBlockY = 0
     }
-    this.currentBlock = arg
-    this.currentBlockX = 1
-    this.currentBlockY = 0
   }
 
   tick() {
+    if(this.currentBlockY >= this.height -1){
+      this.falling = false;
+    }else{
       this.currentBlockY++;
     }
+  }
 
   hasFalling() {
-    return true;
+    return this.falling;
   }
 }
