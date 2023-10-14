9472c57c5af93e2232a7ae3aa1d2c333841f6739
Ville Karvonen <villekarvonen1@gmail.com>
2023-10-14 13:24:31 +0300

    [GREEN]

--- tdd-mooc-tetris/src/RotatingShape.mjs
+++ tdd-mooc-tetris/src/RotatingShape.mjs
@@ -8,5 +8,12 @@ export class RotatingShape {
   toString(){
     return this.wut;
   }
+  rotateRight() {
+    const lines = this.wut.split("\n");
+    const r1 = lines.reduce((a, l) => a + l.charAt(0), '').split('').reverse().join('')  + '\n';
+    const r2 = lines.reduce((a, l) => a + l.charAt(1), '').split('').reverse().join('')  + '\n';
+    const r3 = lines.reduce((a, l) => a + l.charAt(2), '').split('').reverse().join('')  + '\n';
+    return r1+r2+r3;
+  }
   
 }
