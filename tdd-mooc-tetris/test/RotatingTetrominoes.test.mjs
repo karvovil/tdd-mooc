46b19ea8e395fef533471cdbc0b2d20963555a4a
Ville Karvonen <villekarvonen1@gmail.com>
2023-10-18 07:47:56 +0300

    [RED] there's no O-shape

--- tdd-mooc-tetris/test/RotatingTetrominoes.test.mjs
+++ tdd-mooc-tetris/test/RotatingTetrominoes.test.mjs
@@ -84,7 +84,6 @@ describe("The I shape", () => {
   });
 });
 
-/*
 describe("The O shape", () => {
   const shape = Tetromino.O_SHAPE;
 
@@ -116,4 +115,3 @@ describe("The O shape", () => {
     expect(distinctOrientations(shape).size).to.equal(1);
   });
 });
-*/
