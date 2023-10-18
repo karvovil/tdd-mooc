0859f3e2e0ffd982b8b5a8117017b8f045919db7
Ville Karvonen <villekarvonen1@gmail.com>
2023-10-18 07:52:16 +0300

    [GREEN]

--- tdd-mooc-tetris/test/RotatingTetrominoes.test.mjs
+++ tdd-mooc-tetris/test/RotatingTetrominoes.test.mjs
@@ -103,7 +103,7 @@ describe("The O shape", () => {
     );
   });
 
-  xit("cannot be rotated left/counter-clockwise", () => {
+  it("cannot be rotated left/counter-clockwise", () => {
     expect(shape.rotateLeft().toString()).to.equalShape(
       `.OO
        .OO
