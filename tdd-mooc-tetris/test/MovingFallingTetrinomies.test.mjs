8e155abfb48eb829e870d180a51e54371a0e62f6
Ville Karvonen <villekarvonen1@gmail.com>
2023-10-23 10:04:24 +0300

    [RED] can be moved beyond walls

--- tdd-mooc-tetris/test/MovingFallingTetrinomies.test.mjs
+++ tdd-mooc-tetris/test/MovingFallingTetrinomies.test.mjs
@@ -47,4 +47,20 @@ describe("Falling tetromino", () => {
        ..........`
     );
   })
+  it("cannot be moved left beyond the board", () => {
+    board.drop(Tetromino.T_SHAPE);
+    board.moveLeft();
+    board.moveLeft();
+    board.moveLeft();
+    board.moveLeft();
+
+    expect(board.toString()).to.equalShape(
+      `.T........
+       TTT.......
+       ..........
+       ..........
+       ..........
+       ..........`
+    );
+  })
 })
