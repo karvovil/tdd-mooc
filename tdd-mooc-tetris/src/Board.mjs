b90c05511d5ba5631e5be11f7617b43bfef83bd5
Ville Karvonen <villekarvonen1@gmail.com>
2023-10-23 09:53:25 +0300

    [GREEN]

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -65,6 +65,9 @@ export class Board {
   moveLeft(){
     this.current.coords.forEach(c => c.x--);
   }
+  moveRight(){
+    this.current.coords.forEach(c => c.x++);
+  }
   equal(c1, c2){
     return c1.x === c2.x && c1.y === c2.y
   }
