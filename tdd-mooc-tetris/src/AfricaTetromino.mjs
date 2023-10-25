f5dc42216a68f74475e92673c0d3ad714358e708
Ville Karvonen <villekarvonen1@gmail.com>
2023-10-25 10:32:38 +0300

    [REFACTOR] fixed

--- tdd-mooc-tetris/src/AfricaTetromino.mjs
+++ tdd-mooc-tetris/src/AfricaTetromino.mjs
@@ -3,8 +3,21 @@ import { RotatingShape } from "./RotatingShape.mjs";
 export class AfricaTetromino {
 
   static T_SHAPE = new RotatingShape( 
-    `TTT
-     .T.
-     ...`
+    `....
+     TTT.
+     .T..
+     ....`
+  )
+  static I_SHAPE = new RotatingShape( 
+    `....
+     IIII
+     ....
+     ....`
+  )
+  static O_SHAPE = new RotatingShape( 
+    `...
+     .OO.
+     .OO.
+     ....`
   )
 }
