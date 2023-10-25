c3c6897fb9ce9a05cd2fb4b71527dbbc83528e5f
Ville Karvonen <villekarvonen1@gmail.com>
2023-10-25 12:11:08 +0300

    [REFACTOR]

--- tdd-mooc-tetris/src/AfricaTetromino.mjs
+++ tdd-mooc-tetris/src/AfricaTetromino.mjs
@@ -3,50 +3,44 @@ import { RotatingShape } from "./RotatingShape.mjs";
 export class AfricaTetromino {
 
   static T_ROTATIONS = [
-    new RotatingShape(
     `....
      TTT.
      .T..
      ....`
-    ), 
-    new RotatingShape( 
+    , 
     `.T..
      .TT.
      .T..
      ....`
-    ),
-    new RotatingShape( 
+    ,
      `....
       .T..
       TTT.
       ....`
-    ), 
-    new RotatingShape( 
+    , 
      `.T..
       TT..
       .T..
       ....`
-    )] 
+    ] 
   static I_ROTATIONS = [
-    new RotatingShape( 
      `....
       IIII
       ....
       ....`
-    ), 
-    new RotatingShape(
+    , 
      `..I.
       ..I.
       ..I.
       ..I.`
-    )]
-  static O_ROTATIONS = new RotatingShape( 
+    ]
+  static O_ROTATIONS = [
     `....
-     .OO.
-     .OO.
-     ....`
-  )
-  static T_SHAPE = this.T_ROTATIONS[0]
-  static I_SHAPE = this.I_ROTATIONS[0]
-  static O_SHAPE = this.O_ROTATIONS[0]
+    .OO.
+    .OO.
+    ....`
+  ] 
+  static T_SHAPE = new RotatingShape(this.T_ROTATIONS[0])
+  static I_SHAPE = new RotatingShape(this.I_ROTATIONS[0])
+  static O_SHAPE = new RotatingShape(this.O_ROTATIONS[0])
 }
