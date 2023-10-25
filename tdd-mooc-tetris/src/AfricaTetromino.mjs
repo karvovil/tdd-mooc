8f5aeb023baf7a86c4063fb920a7c3ecfef3eb3b
Ville Karvonen <villekarvonen1@gmail.com>
2023-10-25 13:13:29 +0300

    [REFACTOR]

--- tdd-mooc-tetris/src/AfricaTetromino.mjs
+++ tdd-mooc-tetris/src/AfricaTetromino.mjs
@@ -1,46 +1,8 @@
 import { RotatingShape } from "./RotatingShape.mjs";
+import { Rotations } from "./Rotations.mjs";
 
 export class AfricaTetromino {
-
-  static T_ROTATIONS = [
-    `....
-     TTT.
-     .T..
-     ....`
-    , 
-    `.T..
-     .TT.
-     .T..
-     ....`
-    ,
-     `....
-      .T..
-      TTT.
-      ....`
-    , 
-     `.T..
-      TT..
-      .T..
-      ....`
-    ] 
-  static I_ROTATIONS = [
-     `....
-      IIII
-      ....
-      ....`
-    , 
-     `..I.
-      ..I.
-      ..I.
-      ..I.`
-    ]
-  static O_ROTATIONS = [
-    `....
-    .OO.
-    .OO.
-    ....`
-  ] 
-  static T_SHAPE = new RotatingShape(this.T_ROTATIONS[0])
-  static I_SHAPE = new RotatingShape(this.I_ROTATIONS[0])
-  static O_SHAPE = new RotatingShape(this.O_ROTATIONS[0])
+  static T_SHAPE = new RotatingShape(Rotations.T_ROTATIONS[0])
+  static I_SHAPE = new RotatingShape(Rotations.I_ROTATIONS[0])
+  static O_SHAPE = new RotatingShape(Rotations.O_ROTATIONS[0])
 }
---
+++ tdd-mooc-tetris/src/Rotations.mjs
@@ -0,0 +1,27 @@
+
+export class Rotations {
+
+  static T_ROTATIONS = [
+    `....\nTTT.\n.T..\n....\n`,
+    `.T..\n.TT.\n.T..\n....\n`,
+    `....\n.T..\nTTT.\n....\n`, 
+    `.T..\nTT..\n.T..\n....\n`
+    ] 
+  static I_ROTATIONS = [
+     `....
+      IIII
+      ....
+      ....`
+    , 
+     `..I.
+      ..I.
+      ..I.
+      ..I.`
+    ]
+  static O_ROTATIONS = [
+    `....
+    .OO.
+    .OO.
+    ....`
+  ] 
+}
