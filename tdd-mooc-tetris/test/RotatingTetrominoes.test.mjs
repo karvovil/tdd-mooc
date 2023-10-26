fa25a93e76910d8eaead93855c5da019b6b54450
Ville Karvonen <villekarvonen1@gmail.com>
2023-10-26 10:08:50 +0300

    [REFACTOR]

--- tdd-mooc-tetris/test/RotatingTetrominoes.test.mjs
+++ tdd-mooc-tetris/test/RotatingTetrominoes.test.mjs
@@ -71,7 +71,6 @@ describe("The I shape", () => {
   });
 
   it("can be rotated left/counter-clockwise", () => {
-    console.log(shape);
     expect(shape.rotateLeft().toString()).to.equalShape(
       `..I.
        ..I.
