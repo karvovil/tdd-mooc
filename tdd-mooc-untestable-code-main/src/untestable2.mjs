40e0fe7a197c944f3682d49d5885ae1841859975
Ville Karvonen <villekarvonen1@gmail.com>
2023-10-31 04:44:37 +0200

    untestable 2 done

--- tdd-mooc-untestable-code-main/src/untestable2.mjs
+++ tdd-mooc-untestable-code-main/src/untestable2.mjs
@@ -3,7 +3,8 @@ function diceRoll() {
   const max = 6;
   return Math.floor(Math.random() * (max + 1 - min) + min);
 }
-
+//can't assert exact values. let's check that all possible values are returned
+// and that two sufficiently long arrays build from dice rolls are different
 export function diceHandValue() {
   const die1 = diceRoll();
   const die2 = diceRoll();
