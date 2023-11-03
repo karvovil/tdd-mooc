11e5fb17175d9933db654f63f1cd2c9b19825d1a
Ville Karvonen <villekarvonen1@gmail.com>
2023-11-03 09:15:18 +0200

    95% mutation coverage

--- tdd-mooc-legacy-code-main/test/gilded_rose.test.mjs
+++ tdd-mooc-legacy-code-main/test/gilded_rose.test.mjs
@@ -7,93 +7,104 @@ describe("Gilded Rose", () => {
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("foo");
   });
-  it("should increase concert pass quality", () => {
+  it("empty items should be empty", () => {
+    const gildedRose = new Shop([]);
+    const items = gildedRose.updateQuality();
+    expect(items).to.be.an('array').that.is.empty;
+  });
+  it("sellIn should decrease", () => {
     const gildedRose = new Shop(
-      [new Item("Backstage passes to a TAFKAL80ETC concert", 1, 2)
+      [new Item("foo", 3, 3)
     ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.equal(5);
+    expect(items[0].sellIn).to.equal(2);
   });
-  it("should decrease items quality by two", () => {
+  it("should decrease items quality by two if sellIn has passed", () => {
     const gildedRose = new Shop(
-      [new Item("foo", 0, 2)
+      [new Item("foo", 0, 3)
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(1);
+  });
+  it("items quality should not be negative", () => {
+    const gildedRose = new Shop(
+      [new Item("foo", 0, 0)
     ]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).to.equal(0);
   });
-  it("items quality should stay the same if negative", () => {
+  it("Aged Bries quality should increase", () => {
     const gildedRose = new Shop(
-      [new Item("foo", 0, -1)
+      [new Item("Aged Brie", 1, 3)
     ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.equal(-1);
-  });
-  it("should set concert pass quality to 0", () => {
-    const gildedRose = new Shop(
-      [new Item("Backstage passes to a TAFKAL80ETC concert", 0, 3)
-    ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.equal(0);
-
-  });
-  it("Aged Bries quality shuold increase by two", () => {
-    const gildedRose = new Shop(
-      [new Item("Aged Brie", 0, 3)
-    ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.equal(5);
-  });
-  it("Aged Bries sellIn should decrease", () => {
-    const gildedRose = new Shop(
-      [new Item("Aged Brie", 0, 3)
-    ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].sellIn).to.equal(-1);
-  });
-  
-  it("Sulfuras quality should stay the same", () => {
-    const gildedRose = new Shop(
-      [new Item("Sulfuras, Hand of Ragnaros", 0, 3)
-    ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.equal(3);
-  });
-  
-  it("If Aged bries quality is over 50, it should stay the same", () => {
-    const gildedRose = new Shop(
-      [new Item("Aged Brie", 0, 55)
-    ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.equal(55);
+    expect(items[0].quality).to.equal(4);
   });
 
-  it("concert pass quality increases by one if sellin over 11", () => {
+  it("concert pass quality should increase", () => {
     const gildedRose = new Shop(
       [new Item("Backstage passes to a TAFKAL80ETC concert", 15, 2)
     ]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).to.equal(3);
   });
-
-  it("Sulfuras quality should stay the same", () => {
+  it("concert pass quality should increase by 2", () => {
     const gildedRose = new Shop(
-      [new Item("Sulfuras, Hand of Ragnaros", -1, 45)
+      [new Item("Backstage passes to a TAFKAL80ETC concert", 9, 2)
     ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.equal(45);
+    expect(items[0].quality).to.equal(4);
+  });
+  it("concert pass quality should increase by 3", () => {
+    const gildedRose = new Shop(
+      [new Item("Backstage passes to a TAFKAL80ETC concert", 4, 2)
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(5);
+  });
+  it("concert pass quality can't go over 50", () => {
+    const gildedRose = new Shop(
+      [new Item("Backstage passes to a TAFKAL80ETC concert", 2, 49)
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(50);
+  });
+  it("should set concert pass quality to 0", () => {
+    const gildedRose = new Shop(
+      [new Item("Backstage passes to a TAFKAL80ETC concert", 0, 1)
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(0);
+  });
+  
+  it("Sulfuras quality should stay the same", () => {
+    const gildedRose = new Shop(
+      [new Item("Sulfuras, Hand of Ragnaros", 5, 80)
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(80);
   });
   it("Sulfuras sellIn should stay the same", () => {
     const gildedRose = new Shop(
-      [new Item("Sulfuras, Hand of Ragnaros", -1, 45)
+      [new Item("Sulfuras, Hand of Ragnaros", 7, 45)
     ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].sellIn).to.equal(-1);
+    expect(items[0].sellIn).to.equal(7);
   });
-  it("Sulfuras wuality should stay the same", () => {
+  
+  it("Aged brie quality should increase ", () => {
     const gildedRose = new Shop(
-      [new Item("Aged Brie", -1, 45)
+      [new Item("Aged Brie", 0, 45)
     ]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).to.equal(47);
   });
+  it("If Aged bries quality is 50, it won't increase", () => {
+    const gildedRose = new Shop(
+      [new Item("Aged Brie", 0, 50)
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(50);
+  });
+
 });
