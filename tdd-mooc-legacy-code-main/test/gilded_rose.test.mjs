370cc46ea3422ec1ca761010380fe07be834d6de
Ville Karvonen <villekarvonen1@gmail.com>
2023-11-03 07:10:59 +0200

    second test

--- tdd-mooc-legacy-code-main/test/gilded_rose.test.mjs
+++ tdd-mooc-legacy-code-main/test/gilded_rose.test.mjs
@@ -8,9 +8,11 @@ describe("Gilded Rose", () => {
     expect(items[0].name).to.equal("foo");
   });
 
-  it("should lower items quality", () => {
-    const gildedRose = new Shop([new Item("foo", 1, 2)]);
+  it("should increase concert pass quality", () => {
+    const gildedRose = new Shop(
+      [new Item("Backstage passes to a TAFKAL80ETC concert", 1, 2)
+    ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.equal(1);
+    expect(items[0].quality).to.equal(5);
   });
 });
