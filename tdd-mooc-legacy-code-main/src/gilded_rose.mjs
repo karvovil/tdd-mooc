91f87e908b66802f153fa215063adc3a7f02ec71
Ville Karvonen <villekarvonen1@gmail.com>
2023-11-04 07:31:29 +0200

    refactor

--- tdd-mooc-legacy-code-main/src/gilded_rose.mjs
+++ tdd-mooc-legacy-code-main/src/gilded_rose.mjs
@@ -21,6 +21,7 @@ export class Shop {
         case "Sulfuras, Hand of Ragnaros":
           break;
         default:
+          
           break;
       }
 
@@ -29,22 +30,22 @@ export class Shop {
         && this.items[i].quality > 0
         && this.items[i].name != "Sulfuras, Hand of Ragnaros"
       ) {
-        this.items[i].quality = this.items[i].quality - 1;
+        this.items[i].quality--;
       }else{
         if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
+          this.items[i].quality++;
           if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
             if (this.items[i].sellIn < 11 && this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+                this.items[i].quality++;
             }
             if (this.items[i].sellIn < 6 && this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+                this.items[i].quality++;
             }
           }
         }
       }
       if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+        this.items[i].sellIn--
       }
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != "Aged Brie") {
@@ -52,14 +53,14 @@ export class Shop {
             if (this.items[i].quality > 0 
               && this.items[i].name != "Sulfuras, Hand of Ragnaros"
             ) {
-              this.items[i].quality = this.items[i].quality - 1;
+              this.items[i].quality--;
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+            this.items[i].quality = 0;
           }
         } else {
           if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+            this.items[i].quality++;
           }
         }
       }
