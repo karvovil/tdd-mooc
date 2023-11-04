f0bb5acbec14ed9fb8dd53818600dc76fbddb7ab
Ville Karvonen <villekarvonen1@gmail.com>
2023-11-04 07:20:06 +0200

    refactor

--- tdd-mooc-legacy-code-main/src/gilded_rose.mjs
+++ tdd-mooc-legacy-code-main/src/gilded_rose.mjs
@@ -38,10 +38,10 @@ export class Shop {
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != "Aged Brie") {
           if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
+            if (this.items[i].quality > 0 
+              && this.items[i].name != "Sulfuras, Hand of Ragnaros"
+            ) {
+              this.items[i].quality = this.items[i].quality - 1;
             }
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
