9b4cd440ee3940dbff378a894149bf32b71233b6
Ville Karvonen <villekarvonen1@gmail.com>
2023-11-04 07:45:12 +0200

    refactor

--- tdd-mooc-legacy-code-main/src/gilded_rose.mjs
+++ tdd-mooc-legacy-code-main/src/gilded_rose.mjs
@@ -13,26 +13,25 @@ export class Shop {
 
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
+
       switch (this.items[i].name) {
         case "Aged Brie":
           if (this.items[i].quality < 50) {
             this.items[i].quality++;
           }
-        this.items[i].sellIn--
+          this.items[i].sellIn--
           break;
         case "Backstage passes to a TAFKAL80ETC concert":
-          
           if (this.items[i].quality < 50) {
             this.items[i].quality++;
           }
           if (this.items[i].sellIn < 11 && this.items[i].quality < 50) {
             this.items[i].quality++;
-        }
-        if (this.items[i].sellIn < 6 && this.items[i].quality < 50) {
+          }
+          if (this.items[i].sellIn < 6 && this.items[i].quality < 50) {
             this.items[i].quality++;
-        }
-        this.items[i].sellIn--
-
+          }
+          this.items[i].sellIn--
           break;
         case "Sulfuras, Hand of Ragnaros":
           break;
@@ -40,10 +39,10 @@ export class Shop {
           if(this.items[i].quality > 0){
             this.items[i].quality--;
           }
-        this.items[i].sellIn--
+          this.items[i].sellIn--
           break;
       }
-      
+
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != "Aged Brie") {
           if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
