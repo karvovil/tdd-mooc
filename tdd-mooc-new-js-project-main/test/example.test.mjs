0e20a94b17ee5dfd2b1c7bf428f1686e97a0e618
Ville Karvonen <villekarvonen1@gmail.com>
2023-11-06 07:36:51 +0200

    [RED]

--- tdd-mooc-new-js-project-main/test/example.test.mjs
+++ tdd-mooc-new-js-project-main/test/example.test.mjs
@@ -5,4 +5,8 @@ describe("Game", () => {
   it("returns a string", () => {
     expect(game('')).to.be.a('string');
   });
+
+  it("first line of input has only numbers and b, o and $ -chars", () => {
+    expect(() => game('abof\nb2o')).to.throw("not valid");
+  });
 });
