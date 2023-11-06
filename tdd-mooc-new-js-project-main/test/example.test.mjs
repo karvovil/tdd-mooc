0bce7fafd7ce8628901bc9a14e90941260ddedf3
Ville Karvonen <villekarvonen1@gmail.com>
2023-11-06 07:56:54 +0200

    [RED]

--- tdd-mooc-new-js-project-main/test/example.test.mjs
+++ tdd-mooc-new-js-project-main/test/example.test.mjs
@@ -9,4 +9,7 @@ describe("Game", () => {
   it("first line of input has only numbers and b, o and $ -chars", () => {
     expect(() => game('abof\nb2o')).to.throw("not valid");
   });
+  it("returns a string if input string is valid", () => {
+    expect(game('bob$2bo$3o!\n#C ')).to.be.a('string');
+  });
 });
