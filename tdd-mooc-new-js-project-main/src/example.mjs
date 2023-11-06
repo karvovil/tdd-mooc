53ec977f9123b08e9142b81a15c59c9875dc5776
Ville Karvonen <villekarvonen1@gmail.com>
2023-11-06 08:33:57 +0200

    [GREEN]

--- tdd-mooc-new-js-project-main/src/example.mjs
+++ tdd-mooc-new-js-project-main/src/example.mjs
@@ -1,6 +1,8 @@
 export function game(str) {
   const lreChars = ['1','2','3','4','5','6','7','8','9','0','b','o','$']
-  if (lreChars.some(c =>  str.split("\n")[0].includes(c)) ){
+  const strToCheck = str.split('\n')[0];
+  console.log(strToCheck);
+  if ( strToCheck.match(/(?![bo$1234567890!])./g) ){
     throw new Error('not valid')
   }
   return '';
