eecd47d8306f66cdad68cd350c2fb15db23671ae
Ville Karvonen <villekarvonen1@gmail.com>
2023-11-08 04:55:30 +0200

    [REFACTOR] game uses new class

--- tdd-mooc-new-js-project-main/src/example.mjs
+++ tdd-mooc-new-js-project-main/src/example.mjs
@@ -1,7 +1,10 @@
+import { GameState } from "./GameState.mjs";
+
 export function game(str) {
   const strToCheck = str.split('\n')[0];
   if ( strToCheck.match(/(?![bo$1234567890!])./g) ){
     throw new Error('not valid')
   }
-  return '';
+  let gs = new GameState(strToCheck)
+  return gs.toString();
 }
