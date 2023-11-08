1fa5aa6d53228cfc4a6a19dc937e9bfb41b581cf
Ville Karvonen <villekarvonen1@gmail.com>
2023-11-08 06:09:50 +0200

    [REFACTOR]

--- tdd-mooc-new-js-project-main/test/GameState.test.mjs
+++ tdd-mooc-new-js-project-main/test/GameState.test.mjs
@@ -9,7 +9,6 @@ describe("GameState", () => {
 
   it("noNumbers() removes numbers from RLE format", () => {
     let gs = new GameState('b2o$o2b$o2b!')
-    expect(gs.toString()).to.be.a('string');
     expect(gs.noNumbers()).to.not.contain.oneOf(
       ['0','1','2','3','4','5','6','7','8','9',]
     )
