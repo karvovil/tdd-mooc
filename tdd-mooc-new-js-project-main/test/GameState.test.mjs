e85eda7b86f4065078535b4a073b599742da0afe
Ville Karvonen <villekarvonen1@gmail.com>
2023-11-10 09:38:32 +0200

    [REFACTOR] removed test for unused function

--- tdd-mooc-new-js-project-main/test/GameState.test.mjs
+++ tdd-mooc-new-js-project-main/test/GameState.test.mjs
@@ -29,11 +29,6 @@ describe("GameState", () => {
     expect(gs.toArray()).to.deep.equal(['boo','obb','obb']);
   });
   
-  it("arrayToNoNumbers() converts array to noNumbers representation", () => {
-    let gs = new GameState('b2o$o2b$o2b!')
-    expect(gs.arrayToNoNumbers(gs.toArray())).to.equal('boo$obb$obb!');
-  });
-
   it("NoNumbersToString() converts noNumbers representation to string", () => {
     let gs = new GameState('b2o$o2b$o2b!')
     expect(gs.arrayToString(gs.toArray())).to.equal('b2o$o2b$o2b!');
