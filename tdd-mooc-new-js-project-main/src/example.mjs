24dbe5495ed81c51a89245934e50c5c0fdd9557d
Ville Karvonen <villekarvonen1@gmail.com>
2023-11-12 12:49:57 +0200

    [GREEN]

--- tdd-mooc-new-js-project-main/src/example.mjs
+++ tdd-mooc-new-js-project-main/src/example.mjs
@@ -1,10 +1,20 @@
 import { GameState } from "./GameState.mjs";
+import * as fs from 'fs';
+import { readFile } from "node:fs/promises";
 
-export function game(str) {
-  const strToCheck = str.split('\n')[0];
-  if ( strToCheck.match(/(?![bo$1234567890!])./g) ){
+export async function game(inFilePath, n) {
+  const outFilePath = "./out.txt"
+  let contents;
+  try {
+    contents = await readFile(inFilePath, { encoding: "utf8" });
+  } catch (e) {console.log(e)}
+  if ( contents.match(/(?![bo$1234567890!])./g) ){
     throw new Error('not valid')
   }
-  let gs = new GameState(strToCheck)
-  return gs.toString();
+  let gs = new GameState(contents);
+
+  await fs.writeFile(outFilePath, gs.toString(), (err) => {
+    console.log(err || "done");
+  });
+
 }
