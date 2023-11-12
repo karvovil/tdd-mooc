a547af0b5175db287675c102eca8569efd1d2329
Ville Karvonen <villekarvonen1@gmail.com>
2023-11-12 11:34:53 +0200

    [RED] game test expects to use files instead of strings

--- tdd-mooc-new-js-project-main/test/example.test.mjs
+++ tdd-mooc-new-js-project-main/test/example.test.mjs
@@ -1,15 +1,62 @@
 import { expect } from "chai";
 import { game } from "../src/example.mjs";
+import * as fs from 'fs';
 
-describe("Game", () => {
-  it("returns a string", () => {
-    expect(game('')).to.be.a('string');
+const inFilePath = "./build/in.txt"
+const outFilePath = "./build/out.txt"
+
+describe("Game get's valid input", () => {
+  beforeEach(() => {
+    if (!fs.existsSync('./build')){
+      fs.mkdirSync('./build');
+    }
+    const inputContent = 'bob$2bo$3o!\n#C '
+
+    fs.writeFile(inFilePath, inputContent, (err) => {
+      console.log(err || "done");
+    });
   });
 
-  it("first line of input has only numbers and b, o, $ and ! -chars", () => {
-    expect(() => game('abof\nb2o')).to.throw("not valid");
+  afterEach(() => {
+    fs.unlinkSync(inFilePath);
+    fs.unlinkSync(outFilePath);
   });
-  it("returns a string if input string is valid", () => {
-    expect(game('bob$2bo$3o!\n#C ')).to.be.a('string');
+  
+  it("writes output correctly", async () => {
+    game(inFilePath, 0)
+    let contents;
+    try {
+      contents = await readFile(outFilePath, { encoding: "utf8" });
+    } catch (e) {console.log(e)}
+
+    expect(contents).to.equal('bob$2bo$3o!\n#C ');
+  });
+});
+
+describe("Game get's invalid input", () => {
+  let gs;
+  beforeEach(() => {
+    if (!fs.existsSync('./build')){
+      fs.mkdirSync('./build');
+    }
+    const inputContent = 
+    'abof\nb2o'
+
+    fs.writeFile(inFilePath, inputContent, (err) => {
+      console.log(err || "done");
+    });
+    game(inFilePath, 0);
+  });
+  afterEach(() => {
+    fs.unlinkSync(inFilePath);
+  });
+  
+  it("throws error", async () => {
+    let contents;
+    try {
+      contents = await readFile(outFilePath, { encoding: "utf8" });
+    } catch (e) {console.log(e)}
+
+    expect(() => game(inFilePath, 0)).to.throw("not valid");
   });
 });
