1210c30c467f40f30154f965d44396d233ee5454
Ville Karvonen <villekarvonen1@gmail.com>
2023-11-12 12:00:05 +0200

    [RED] one test at a time

--- tdd-mooc-new-js-project-main/test/example.test.mjs
+++ tdd-mooc-new-js-project-main/test/example.test.mjs
@@ -1,16 +1,17 @@
 import { expect } from "chai";
 import { game } from "../src/example.mjs";
 import * as fs from 'fs';
+import { readFile } from "node:fs/promises";
 
 const inFilePath = "./build/in.txt"
-const outFilePath = "./build/out.txt"
+const outFilePath = "./out.txt"
 
 describe("Game get's valid input", () => {
   beforeEach(() => {
     if (!fs.existsSync('./build')){
       fs.mkdirSync('./build');
     }
-    const inputContent = 'bob$2bo$3o!\n#C '
+    const inputContent = 'bob$2bo$3o!'
 
     fs.writeFile(inFilePath, inputContent, (err) => {
       console.log(err || "done");
@@ -23,17 +24,17 @@ describe("Game get's valid input", () => {
   });
   
   it("writes output correctly", async () => {
-    game(inFilePath, 0)
+    await game(inFilePath, 0)
     let contents;
     try {
       contents = await readFile(outFilePath, { encoding: "utf8" });
     } catch (e) {console.log(e)}
 
-    expect(contents).to.equal('bob$2bo$3o!\n#C ');
+    expect(contents).to.equal('bob$2bo$3o!');
   });
 });
 
-describe("Game get's invalid input", () => {
+/* describe("Game get's invalid input", () => {
   let gs;
   beforeEach(() => {
     if (!fs.existsSync('./build')){
@@ -45,7 +46,6 @@ describe("Game get's invalid input", () => {
     fs.writeFile(inFilePath, inputContent, (err) => {
       console.log(err || "done");
     });
-    game(inFilePath, 0);
   });
   afterEach(() => {
     fs.unlinkSync(inFilePath);
@@ -59,4 +59,4 @@ describe("Game get's invalid input", () => {
 
     expect(() => game(inFilePath, 0)).to.throw("not valid");
   });
-});
+}); */
