e71507e2b1b653b81cf1da8c3fcb18271e331fff
Ville Karvonen <villekarvonen1@gmail.com>
2023-11-13 07:42:32 +0200

    [REFACTOR] using synchronous read/write functions

--- tdd-mooc-new-js-project-main/src/example.mjs
+++ tdd-mooc-new-js-project-main/src/example.mjs
@@ -1,19 +1,18 @@
 import { GameState } from "./GameState.mjs";
 import * as fs from 'fs';
-import { readFile } from "node:fs/promises";
 
-export async function game(inFilePath, n) {
+export function game(inFilePath, n) {
   const outFilePath = "./out.txt"
   let contents;
   try {
-    contents = await readFile(inFilePath, { encoding: "utf8" });
+    contents = fs.readFileSync(inFilePath, { encoding: "utf8" });
   } catch (e) {console.log(e)}
   if ( contents.match(/(?![bo$1234567890!])./g) ){
     throw new Error('not valid')
   }
   let gs = new GameState(contents);
 
-  await fs.writeFile(outFilePath, gs.toString(), (err) => {
+  fs.writeFileSync(outFilePath, gs.toString(), (err) => {
     console.log(err || "done");
   });
 
