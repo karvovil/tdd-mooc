9c416816cffe0c9ee27d655db0556e0e4fced5fb
Ville Karvonen <villekarvonen1@gmail.com>
2023-11-13 07:43:39 +0200

    [REFACTOR] moving to synchronoys code

--- tdd-mooc-new-js-project-main/test/example.test.mjs
+++ tdd-mooc-new-js-project-main/test/example.test.mjs
@@ -1,7 +1,6 @@
 import { expect } from "chai";
 import { game } from "../src/example.mjs";
 import * as fs from 'fs';
-import { readFile } from "node:fs/promises";
 
 const inFilePath = "./build/in.txt"
 const outFilePath = "./out.txt"
@@ -11,7 +10,7 @@ describe("Game get's valid input", () => {
 
     const inputContent = 'bob$2bo$3o!'
 
-    fs.writeFile(inFilePath, inputContent, (err) => {
+    fs.writeFileSync(inFilePath, inputContent, (err) => {
       console.log(err || "done");
     });
   });
@@ -19,11 +18,11 @@ describe("Game get's valid input", () => {
   afterEach(() => {
   });
   
-  it("writes output correctly", async () => {
-    await game(inFilePath, 0)
+  it("writes output correctly", () => {
+    game(inFilePath, 0);
     let contents;
     try {
-      contents = await readFile(outFilePath, { encoding: "utf8" });
+      contents = fs.readFileSync(outFilePath, { encoding: "utf8" });
     } catch (e) {console.log(e)}
 
     expect(contents).to.equal('bob$2bo$3o!');
